a909e071f0369eeccf516db629e8f560f1e3b43b
Guy Harel <guy@remarkety.com>
2019-02-10 12:01:37 +0200

    Initial commit - working code

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # serverless-s3-thumbnails
-Automated image resize service that creates thumbnails for images on S3
+A serverless lambda ImageMagick-based S3 thumbnail creator.
+
+## Overview
+As opposed to the usual S3 resizers which use imagemagick and create thumbnails
+in a separate bucket, this project uses a single bucket, with one "top-level" directory
+being the source, and another being the target.
+
+This project also deletes the thumbnail if the original image is removed.
+
+The "source" directory needs to be "top-level", ie no slashes.
+The "destination" directory will be the same as "source", plus an additional prefix (default is "-thumb").
+
+For example, if the image was uploaded into `<bucket>:/src/dir1/dir2/image.jpg`, the thumbnail
+will be saved to: `<bucket>:/src-thumb/dir1/dir2/image.jpg`;
+
+## Configuration
+The function accepts the following configuration via environment variables:
+
+| Variable | Purpose | Default Value |
+|----------|---------|---------------|
+|`THUMB_DIR_SUFFIX`| The suffix to append to the src dir in order to set the dstDir | -thumb |
+|`WIDTH` | The thumbnail max width | 100 |
+|`HEIGHT` | The thumbnail max height | 100 |
+
+## Deployment
+Install dependencied and deploy:
+```
+npm install
+serverless deploy
+```
+
+Once deployed, you need to setup your own S3 triggers on the lambda function.
+This is because `servless` doesn't support creating triggers on existing buckets very well.
+
+- To enable creation of thumbnails, enable the "S3 / ObjectCreated" trigger.
+- To automatically delete thumbnails when the originals are removed, enable the "S3 / OBjectRemoved" trigger as well. 
+
+You also need to make sure that the lamda role includes `GetObject` and `PutObject` permissions on the bucket. 
+Here's an example policy:
+
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::my-image-bucket",
+                "arn:aws:s3:::my-image-bucket/*"
+            ]
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetAccountPublicAccessBlock",
+                "s3:ListAllMyBuckets",
+                "s3:HeadBucket"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+```
+ 
---
+++ handler.js
@@ -0,0 +1,107 @@
+// dependencies
+const AWS = require('aws-sdk');
+const util = require('util');
+const gm = require('gm').subClass({ imageMagick: true });
+require('gm-base64');
+const s3 = new AWS.S3();
+
+const performResize = (buffer, imageType, setWidth, setHeight) => {
+    console.log("performResize to " + setWidth + " x " + setHeight);
+    return new Promise ( (resolve, reject) => {
+        gm(buffer).size(function(err, size) {
+            // Infer the scaling factor to avoid stretching the image unnaturally.
+            var scalingFactor = Math.min(
+                setWidth / size.width,
+                setHeight / size.height
+            );
+            var width  = scalingFactor * size.width;
+            var height = scalingFactor * size.height;
+
+            // Transform the image buffer in memory.
+            this.resize(width, height)
+                .toBuffer(imageType, function(err, buffer) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({
+                            mime: imageType,
+                            buffer: buffer
+                        });
+                    }
+                });
+        });
+    });
+}
+
+module.exports.resize = async (event, context) => {
+    return new Promise( async (resolve, reject) => {
+        // Read options from the event.
+        console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+
+        let thumbDirSuffix = process.env.THUMB_DIR_SUFFIX;
+
+        const thumbWidth = process.env.WIDTH == undefined ? 100 : process.env.WIDTH;
+        const thumbHeight = process.env.HEIGHT == undefined ? 100 : process.env.HEIGHT;
+
+        if (typeof(thumbDirSuffix) == "undefined")
+            thumbDirSuffix = "-thumb";
+
+        const srcBucket = event.Records[0].s3.bucket.name;
+        // Object key may have spaces or unicode non-ASCII characters.
+        const srcKey    =
+            decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+        const srcKeyParts = srcKey.split("/");
+        const srcRootDirectory = srcKeyParts[0];
+        if (srcRootDirectory.endsWith(thumbDirSuffix)) {
+            console.log("Received an event for the thumbnail directory - ignoring")
+            return;
+        }
+        const dstDirectory = srcRootDirectory + thumbDirSuffix;
+        console.log(srcKeyParts);
+        console.log(srcKeyParts.slice(1));
+
+        const dstKey = [dstDirectory].concat(srcKeyParts.slice(1)).join('/');
+
+        // Infer the image type.
+        const typeMatch = srcKey.match(/\.([^.]*)$/);
+        if (!typeMatch) {
+            return { message: 'Could not determine the image type.'};
+        }
+        const imageType = typeMatch[1];
+        if (imageType != "jpg" && imageType != "png" && imageType != "gif") {
+            return { message: 'Unsupported image type: ${imageType}'};
+        }
+
+        const eventName = event.Records[0].eventName;
+        if (eventName == 'ObjectCreated:Put') {
+            console.log("Resizing " + srcKey + " into " + dstKey);
+
+            const options = {
+                Bucket: srcBucket,
+                Key: srcKey
+            };
+            const s3Object = await s3.getObject(options).promise();
+
+            const resized = await performResize(s3Object.Body, imageType, thumbWidth, thumbHeight);
+            console.log("Uploading to s3://" + srcBucket + ":" + dstKey);
+
+            await s3.putObject({
+                Bucket: srcBucket,
+                Key: dstKey,
+                ContentType: resized.mime,
+                Body: resized.buffer
+            }).promise();
+
+            console.log("Resize successful");
+        }
+        if (eventName == 'ObjectRemoved:Delete') {
+            console.log(srcKey + " was deleted - removing thumbnail from " + dstKey);
+            await s3.deleteObject({
+                Bucket: srcBucket,
+                Key: dstKey
+            }).promise();
+            console.log("Thumbnail deleted");
+        }
+        resolve(true);
+    });
+};
